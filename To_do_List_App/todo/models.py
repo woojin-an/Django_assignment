e5281fa60839500f9af6ec102ca1037cf4384c0c
woojin-an <dkswoojin@gmail.com>
2024-08-28 00:30:42 +0900

    Django 4일차 과제 제출 (Todolist 기능 추가) (페이지네이션, 제목/내용 검색, 로그인/로그아웃 시 노출 기능, 글쓰기, 수정, 삭제)

--- To_do_List_App/todo/models.py
+++ To_do_List_App/todo/models.py
@@ -1,9 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+User = get_user_model()
 
 class Todo(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     start_date = models.DateField()
     end_date = models.DateField()
     is_completed = models.BooleanField(default=False)
--- To_do_List_App/todo/views.py
+++ To_do_List_App/todo/views.py
@@ -1,25 +1,88 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
+from django.urls import reverse
+from django.views.decorators.http import require_http_methods
+
+from todo.forms import TodoForm
 from todo.models import Todo
 
 
+@login_required
 def todo_list(request):
-    todo_list = Todo.objects.all().values_list('id', 'title')
-    result = [{'id': todo[0], 'title': todo[1]} for i, todo in enumerate(todo_list)]
+    todo_list = Todo.objects.all()
+    q = request.GET.get('q')
+    if q:
+        # 제목과 내용으로 검색할 수 있도록 필터설정
+        todo_list = todo_list.filter(
+            Q(title__icontains=q) |
+            Q(description__icontains=q)
+        )
 
-    return render(request, 'todo_list.html', {'data': result})
+    paginator = Paginator(todo_list, 10)
+    page = request.GET.get('page')
+    page_obj = paginator.get_page(page)
+
+    context = {
+        'page_obj': page_obj,
+        'todo_list': todo_list,
+
+    }
+
+    return render(request, 'todo_list.html', context)
 
 
+@login_required
 def todo_info(request, todo_id):
-    try:
-        todo = Todo.objects.get(id=todo_id)
-        info = {
-            'title': todo.title,
-            'description': todo.description,
-            'start_date': todo.start_date,
-            'end_date': todo.end_date,
-            'is_completed': todo.is_completed,
-        }
-        return render(request, 'todo_info.html', {'data': info})
-    except Todo.DoesNotExist:
-        raise Http404('Todo does not exist')
+    todo = get_object_or_404(Todo, id=todo_id)
+    context = {
+        'id': todo.id,
+        'title': todo.title,
+        'description': todo.description,
+        'start_date': todo.start_date,
+        'end_date': todo.end_date,
+        'is_completed': todo.is_completed,
+        'todo': todo,
+    }
+
+    return render(request, 'todo_info.html', context)
+
+
+@login_required
+def todo_create(request):
+    form = TodoForm(request.POST or None)
+    if form.is_valid():
+        todo = form.save(commit=False)
+        todo.user = request.user
+        todo.save()
+        return redirect(reverse('todo_list'))
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'todo_create.html', context)
+
+
+@login_required     # 버전 관리(revision), 작성 중 저장(db 저장..?), 뒤로가기(목록), 변경사항(다른 점)
+def todo_update(request, todo_id):
+    todo = get_object_or_404(Todo, id=todo_id)
+    form = TodoForm(request.POST or None, instance=todo)
+    if form.is_valid():
+        todo = form.save()
+        return redirect(reverse('todo_list'))
+    context = {
+        'form': form,
+        'todo': todo,
+    }
+    return render(request, 'todo_update.html', context)
+
+
+@login_required
+@require_http_methods(['POST'])
+def todo_delete(request, todo_id):
+    todo = get_object_or_404(Todo, id=todo_id)
+    todo.delete()
+    return redirect(reverse('todo_list'))
