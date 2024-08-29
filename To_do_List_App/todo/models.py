4d927d3462bb826e97f1d3d327b890d84fee0d4f
woojin-an <dkswoojin@gmail.com>
2024-08-29 21:01:11 +0900

    Django 6일차 과제 업로드 (Todolist - Comment 모델, Bootstrap5, pagination)

--- To_do_List_App/todo/models.py
+++ To_do_List_App/todo/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 User = get_user_model()
 
+
 class Todo(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
@@ -15,3 +16,14 @@ class Todo(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    message = models.TextField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}: {self.message}'
