36456092fd8a680a8dca9c876a9e3370f4a4844b
woojin-an <dkswoojin@gmail.com>
2024-09-03 02:19:25 +0900

    Django 8일차 과제 커밋 (CustomUser, 환경변수 설정 - 회원가입시 이메일 인증)

--- To_do_List_App/users/models.py
+++ To_do_List_App/users/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, *args, **kwargs):
+        if not email:
+            raise ValueError('이메일을 입력하세요.')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.is_active = False
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, *args, **kwargs):
+        user = self.create_user(email, password, *args, **kwargs)
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser, PermissionsMixin):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=False)  # 추후 인증을 통해 True로 변환시킴
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def username(self):
+        return self.name
