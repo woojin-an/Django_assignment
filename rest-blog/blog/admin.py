da34ec9f205abeffa4ca99a09130ab49fa19dcbb
woojin-an <dkswoojin@gmail.com>
2024-09-09 16:29:36 +0900

    Django 12일차 학습내용(완료) 커밋

--- rest-blog/blog/admin.py
+++ rest-blog/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Blog
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Blog
--- rest-blog/config/settings.py
+++ rest-blog/config/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
 
     # own
     'blog',
+
+    'rest_framework',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+APPEND_SLASH = False
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+
+}
