e13478d4e5f1fb2130e844b9a949b07e3f7cf1a6
woojin-an <dkswoojin@gmail.com>
2024-09-10 17:15:44 +0900

    Django 13일차 학습내용 커밋 (DRF APIView, DRF Generics)

---
+++ rest-blog/blog/urls/view_set_urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from blog import views
+from blog.views import api_view_set_views
+
+app_name = 'view_set_api'
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'users', api_view_set_views.UserViewSet, basename='user')
+router.register(r'blogs', api_view_set_views.BlogViewSet, basename='blog')
+urlpatterns = [
+    # path('', api_views.blog_list, name='blog_list'),
+    path('', include(router.urls)),
+]
