076c47f58ed1dec27000c03b2575be347f91cf33
woojin-an <dkswoojin@gmail.com>
2024-09-11 18:51:00 +0900

    Django_Restaurant : reviews/views.py , users/views.py, url 연결 완료

--- Django_restaurant/users/views.py
+++ Django_restaurant/users/views.py
@@ -1,36 +1,49 @@
-from rest_framework import viewsets
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework import status, viewsets
 from django.contrib.auth.views import LogoutView
-from .models import User  # 가정: 사용자 모델이 User로 정의되어 있음
-# from serializers import UserSerializer
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import UserDetailSerializer, UserSerializer
+
+User = get_user_model()
 
 
-class UserViewSet(viewsets.ViewSet):
-    def create(self, request):
-        # 회원가입 처리 로직
-        pass
-
-    def list(self, request):
-        # 사용자 목록 조회 로직
-        pass
-
-    def retrieve(self, request, pk=None):
-        # 특정 사용자 조회 로직
-        pass
+class UserSignupView(CreateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = UserSerializer
 
 
-class UserLoginView(viewsets.ViewSet):
-    def create(self, request):
-        # 로그인 처리 로직
-        pass
+class UserLoginView(APIView):
+
+    def post(self, request, *args, **kwargs):
+        # 시리얼라이저로 유저 정보를 보냄
+        serializer = UserSerializer(data=request.data)
+
+        # 시리얼라이저로 유효하지 않다면
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # 유효하다면 그 검증된 데이터(메일,암호) 가져옴
+        email = serializer.validated_data.get('email')
+        password = serializer.validated_data.get('password')
+
+        # 해당 이메일과 암호로 요청보냄
+        user = authenticate(request, email=email, password=password)
+
+        # 유저가 없다면 HTTP400
+        if user is None:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # 유저가 있다면 로그인
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class UserSignupView(viewsets.ViewSet):
-    def create(self, request):
-        # 회원가입 처리 로직
-        pass
+class UserDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserDetailSerializer
 
-
-class UserDetailView(viewsets.ViewSet):
-    def retrieve(self, request, pk=None):
-        # 사용자 프로필 조회 로직
-        pass
+    def get_object(self):
+        return self.request.user
